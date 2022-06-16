a834d6ca05eaa64aa5ead069222f051ae4388fd3
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-16 10:04:56 -0500

    added image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg';
+
 function About() {
     return (
-        <section>
+        <section className= "my-5">
             <h1 id="about"> Who am I?</h1>
+          <img src={coverImage} class name="my-2" style = {{ width: "100%"}} alt="cover" /> 
         </section>
     )
 }
